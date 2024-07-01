d2686b92c92b33ad23dd9eb1b9312adc8851699c
AStar <sxw59941513@163.com>
2024-07-01 10:21:55 +0800

    [modify] Change compiler from .bat to .ps1

---
+++ shaders/shader-complier.ps1
@@ -0,0 +1,15 @@
+# 设置变量
+$folder = "D:\C++\Projects\PathTracer\shaders"
+$compiler = "D:\Vulkan\SDK\1.3.268.0\Bin\glslangValidator.exe"
+$extensions = @("*.frag", "*.vert", "*.rgen", "*.rmiss", "*.rchit")
+
+# 获取所有匹配的文件
+$files = Get-ChildItem -Path $folder -Recurse -Include $extensions
+
+# 遍历每个文件并调用编译器
+foreach ($file in $files) {
+    $outputFile = "$($file.FullName).spv"
+    Write-Host "$compiler --target-env vulkan1.2 $file -o $outputFile"
+    & $compiler --target-env vulkan1.2 $file.FullName -o $outputFile
+    Write-Host ""
+}
